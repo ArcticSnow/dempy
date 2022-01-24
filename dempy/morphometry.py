ab082e115383a0f5de5a453c25030929fa4860cf
arcticsnow <sfilhol@protonmail.com>
2022-01-24 20:11:52 -0300

    added curvature functions

--- dempy/morphometry.py
+++ dempy/morphometry.py
@@ -9,6 +9,7 @@ Geometry, morphology of surface
 
 """
 import numpy as np
+import cv2
 
 
 def slope(Z, dx, neighbor=None):
@@ -83,6 +84,56 @@ def gaussian_curvature(Z, neighbor=None):
     K = (Zxx * Zyy - (Zxy ** 2)) / (1 + (Zx ** 2) + (Zy **2)) ** 2
     return K
     
+
+
+
+def laplacian_of_gaussian_operator(n, sigma, print_sum=False):
+    '''
+    Function to compute operator Laplacian of Gaussian for a "smooth" curvature computation
+    See https://homepages.inf.ed.ac.uk/rbf/HIPR2/log.htm
+
+    :param n:       number of pixel. Ideally use an odd number
+    :param sigma:   standard deviation to use for gaussian curve. This sets the scale at which 
+                    curvature computatino occurs. Make sure n is large enough
+    :return:        n*n array operator
+    '''
+
+    def lapogaus(x, y, sigma):
+        '''
+        Laplacian of Gaussian operator for curvature computation smoothing noise.
+        https://homepages.inf.ed.ac.uk/rbf/HIPR2/log.htm
+        
+        :param x:       array of x-coordinate. Should be centered on 0.
+        :param y:       array of y-coordinate. Should be centered on 0.
+        :param sigma:   float, standard deviation setting the with of the bell curve
+        :return: array containing the operator
+        '''
+        op = -1/(np.pi * sigma **4)*(1-((x**2 + y**2)/(2*sigma**2)))*np.exp(-((x**2 + y**2)/(2*sigma**2)))
+        return op
+
+    x = np.arange(-n,n)
+    y = np.arange(-n,n)
+    Xs, Ys = np.meshgrid(x,y)
+    kernel = lapogaus(Xs,Ys,sigma)
+    if print_sum:
+        print('Kernel sum = ',np.sum(kernel))
+    return kernel
+
+def plot_kernel(kernel):
+    plt.figure()
+    plt.imshow(kernel)
+    plt.colorbar()
+
+def convolve_array(arr, kernel):
+    '''
+    function to convolve a 2D array with a kernel
+    '''
+    res = cv2.filter2D(arr, -1, kernel)
+    return res
+
+
+
+
 
 #def aspect(Z):
  #   Zy, Zx = np.gradient(Z)                                                                                                    
